1ebf02e2c77b37fb767fdb1b5b95152da696655f
Bogdan Lica <bogdanlica74@gmail.com>
2019-02-03 16:52:57 +0000

    Use Model Factories to generate fake data

--- laravel/app/Question.php
+++ laravel/app/Question.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
-    protected $fillable = ['title','body']
+    protected $fillable = ['title','body'];
 
 
     public function user(){
-        return $this->belongsTo(User::class)
+        return $this->belongsTo(User::class);
     }
 
     public function setTitleAttribute($value){
